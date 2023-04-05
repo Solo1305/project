d2b66ae51c6143dd6b94e5c8599d2d1576522405
Solo1305 <116720176+Solo1305@users.noreply.github.com>
2023-04-05 17:50:37 +0100

    Add files via upload

---
+++ backend/routes/product.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getProducts,
+    createProduct,
+    getSingleProduct,
+  } = require("../controllers/productController");
+  router.route("/products").get(getProducts);
+  router.route("/product/new").post(createProduct);
+  router.route("/product/:id").get(getSingleProduct);
+
+module.exports = router;
